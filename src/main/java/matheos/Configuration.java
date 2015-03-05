73d87b4ac27f447fbccac1b3835bda0b6f7f3935
fbillioud <darksharcoux@hotmail.com>
2015-03-05 13:32:34 +0100

    change version name

--- src/main/java/matheos/Configuration.java
+++ src/main/java/matheos/Configuration.java
@@ -63,8 +63,8 @@ import matheos.utils.managers.FontManager;
  */
 public final class Configuration {
 
-    private static final String VERSION = "1.4.3";
-    private static final Integer ID_VERSION = 15;
+    private static final String VERSION = "1.4.4";
+    private static final Integer ID_VERSION = 16;
     private static final String ADRESSE_SITE = "http://lecoleopensource.fr/matheos/";
     public static final String APPLICATION_ICON = "images/matheos.png";
 
